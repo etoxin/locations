de1d34b3dcbec9860b39dffeae49202e86954f4f
etoxin <adam@etoxin.net>
2013-08-25 23:15:23 +1000

    trying out directives

--- scripts/locations.js
+++ scripts/locations.js
@@ -6,16 +6,33 @@ angular.module('locations', []).
 		$routeProvider.
 			when('/', {controller:GalleryCtrl, templateUrl:'/partials/home.html'}).
 			otherwise({redirectTo:'/'});
+	}).
+	directive('mediaItemDirective', function() {
+	  return {
+	  	controller: function ($scope) {
+			if($scope.$last){
+				console.log('Ready!');
+				console.log( $('#item-bedlam_bay').height() );
+			}			
+	  	},
+	  	template:
+	  		'<div id="item-{{item.name}}" class="location-item">'+
+			'	<h2>{{item.name}}</h2>'+
+			'	<img src="{{item.imagePath}}" alt="{{item.name}}" width="100%">'+
+			'	<audio src="{{item.audioPath}}" id="audio-{{item.name}}" controls loop>'+
+			'		<p>Your browser does not support the audio element. Please upgrade.</p>'+
+			'	</audio>'+
+			'</div>'
+	  };
 	});
 
 
 function GalleryCtrl($scope, $http) {
-	//define vars
-
 	$scope.audio = function () {
 		var items = $('.location-item');
+		console.log(items);
 		$.each( items, function(index, value) {
-		  console.log(value);
+		  // console.log(value);
 		});
 	}
 
@@ -32,23 +49,29 @@ function GalleryCtrl($scope, $http) {
 
 
 
+	// var item = $('#item--medowbank'),
+	// 	scrollPosition = $('body').scrollTop(),
+	// 	itemPosition = $(item).offset(),
+	// 	itemHeight = item.height();
+	// if( scrollPosition >= itemPosition.top && scrollPosition <= (itemPosition.top + itemHeight) ){
+	// 	document.getElementById('audio-medowbank').play();
+	// }
+	// if( scrollPosition < itemPosition.top || scrollPosition > (itemPosition.top + itemHeight) ){
+	// 	document.getElementById('audio-medowbank').pause();
+	// }
 
 
-	// run onscroll
-	window.onscroll = function (oEvent) {
-		var item = $('#item-medowbank'),
-			scrollPosition = $('body').scrollTop(),
-			itemPosition = item.offset(),
-			itemHeight = item.height();
-
-		// console.log('scroll position '+scrollPosition);
-		// console.log('item position: '+itemPosition.top);
-		
-		if( scrollPosition >= itemPosition.top && scrollPosition <= (itemPosition.top + itemHeight) ){
-			document.getElementById('audio-medowbank').play();
-		}
-		if( scrollPosition < itemPosition.top || scrollPosition > (itemPosition.top + itemHeight) ){
-			document.getElementById('audio-medowbank').pause();
-		}
-	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
